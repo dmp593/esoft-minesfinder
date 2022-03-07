dc2c583450c58c8a8a8cbdd290f253f52b1faf2d
DANPINT <d.pinto@kigroup.de>
2022-03-07 10:32:48 +0000

    adds MinesFinder main JFrame

---
+++ src/main/java/MinesFinder.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MinesFinder extends JFrame {
+
+    private JPanel painelPrincipal;
+    private JButton btnJogoMedio;
+    private JButton btnSair;
+    private JButton btnJogoFacil;
+    private JButton btnJogoDificil;
+    private JLabel lblJogadorFacil;
+    private JLabel lblPontuacaoFacil;
+    private JLabel lblJogadorMedio;
+    private JLabel lblPontuacaoMedio;
+    private JLabel lblJogadorDificil;
+    private JLabel lblPontuacaoDificil;
+
+    public MinesFinder(String title) {
+        super(title);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(painelPrincipal);
+
+        btnJogoFacil.addActionListener(this::btnJogoFacilActionPerformed);
+        btnJogoMedio.addActionListener(this::btnJogoMedioActionPerformed);
+        btnJogoDificil.addActionListener(this::btnJogoDificilActionPerformed);
+
+        btnSair.addActionListener(this::btnSairActionPerformed);
+
+        pack();
+    }
+
+    private void btnJogoFacilActionPerformed(ActionEvent e) {
+        new JanelaDeJogo();
+    }
+
+    private void btnJogoMedioActionPerformed(ActionEvent e) {
+        // new JanelaDeJogo();
+    }
+
+    private void btnJogoDificilActionPerformed(ActionEvent e) {
+        // new JanelaDeJogo();
+    }
+
+    private void btnSairActionPerformed(ActionEvent e) {
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        new MinesFinder("Mines Finder").setVisible(true);
+    }
+
+}
